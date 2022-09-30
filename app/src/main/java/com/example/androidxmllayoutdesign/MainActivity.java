2854d11cddb5950a97205bfd9892135b5cf264a8
Rakib Ahmed <rakibofficial@gmail.com>
2022-09-30 19:03:06 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/androidxmllayoutdesign/MainActivity.java
+++ app/src/main/java/com/example/androidxmllayoutdesign/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_health_score);
+        setContentView(R.layout.activity_signup);
     }
 }
